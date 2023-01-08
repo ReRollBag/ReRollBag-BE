bbb578c944949c918826c10591f6d2cf3bee3087
donghwan <wkazxf@gmail.com>
2023-01-08 17:34:21 +0900

    [FEAT] Initialize

---
+++ src/main/java/com/ReRollBag/ReRollBagApplication.java
@@ -0,0 +1,13 @@
+package com.ReRollBag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReRollBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReRollBagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReRollBag/ReRollBagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReRollBag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReRollBagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
