19c04301cb6bd96059a52a6e874b91c4d9b010b0
donghwan <wkazxf@gmail.com>
2023-03-02 15:13:30 +0900

    [FEAT] Adding CreatedAt, UpdatedAt with Using JpaAuditing  (RRB-18)

--- src/main/java/com/ReRollBag/domain/entity/BaseTimeEntity.java
+++ src/main/java/com/ReRollBag/domain/entity/BaseTimeEntity.java
@@ -1,7 +1,6 @@
 package com.ReRollBag.domain.entity;
 
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -12,7 +11,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-    @CreatedBy
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @LastModifiedDate
