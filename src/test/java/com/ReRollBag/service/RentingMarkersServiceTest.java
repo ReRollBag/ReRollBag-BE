422fcc4f8785891d5c8e83eafbdb51f4b2103184
donghwan <wkazxf@gmail.com>
2023-03-19 14:41:27 +0900

    [FEAT] Renting Markers Save Test Code (RRB-36)

--- src/test/java/com/ReRollBag/service/RentingMarkersServiceTest.java
+++ src/test/java/com/ReRollBag/service/RentingMarkersServiceTest.java
@@ -8,7 +8,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class RentingMarkersServiceTest {
@@ -20,7 +25,7 @@ public class RentingMarkersServiceTest {
     private RentingMarkersRepository rentingMarkersRepository;
 
     @Test
-    @DisplayName("[Service] RentingMarkers 생성 테스트")
+    @DisplayName("[Service] 대여소 생성 테스트")
     public void Service_대여소생성_테스트() throws Exception {
         //given
         RentingMarkersSaveRequestDto requestDto = RentingMarkersSaveRequestDto.builder()
@@ -34,7 +39,7 @@ public class RentingMarkersServiceTest {
         rentingMarkers.setMarkersId(1L);
 
         //when
-        when(rentingMarkersRepository.save(any())).willReturn(rentingMarkers);
+        when(rentingMarkersRepository.save(any())).thenReturn(rentingMarkers);
         RentingMarkersResponseDto responseDto = rentingMarkersService.save(requestDto);
 
         //then
@@ -46,4 +51,60 @@ public class RentingMarkersServiceTest {
         assertThat(responseDto.getCurrentBagsNum()).isEqualTo(5);
     }
 
+    @Test
+    @DisplayName("[Service] 대여소 가방 개수 줄이기 테스트")
+    public void Service_대여소_가방개수줄이기_테스트() throws Exception {
+        //given
+        RentingMarkers rentingMarkers = RentingMarkers.builder()
+                .markersId(1L)
+                .latitude(12345.54321)
+                .longitude(54321.12345)
+                .name("GS25 우만점")
+                .maxBagsNum(5)
+                .currentBagsNum(5)
+                .build();
+
+        //when
+        when(rentingMarkersRepository.findById(1L)).thenReturn(rentingMarkers);
+        rentingMarkersService.decreaseCurrentBagsNum(1L);
+
+        //then
+        assertThat(rentingMarkers.getCurrentBagsNum()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("[Service] 대여소 전체조회 테스트")
+    public void Service_대여소_전체조회_테스트() throws Exception {
+        //given
+        RentingMarkers rentingMarkers1 = RentingMarkers.builder()
+                .markersId(1L)
+                .latitude(12345.54321)
+                .longitude(54321.12345)
+                .name("GS25 우만점")
+                .maxBagsNum(5)
+                .currentBagsNum(5)
+                .build();
+
+        RentingMarkers rentingMarkers2 = RentingMarkers.builder()
+                .markersId(2L)
+                .latitude(12345.54321)
+                .longitude(54321.12345)
+                .name("GS25 아주대삼거리점")
+                .maxBagsNum(8)
+                .currentBagsNum(8)
+                .build();
+
+        List<RentingMarkers> rentingMarkersList = new ArrayList<>();
+        rentingMarkersList.add(rentingMarkers1);
+        rentingMarkersList.add(rentingMarkers2);
+
+        //when
+        when(rentingMarkersRepository.findAll()).thenReturn(rentingMarkersList);
+        List<RentingMarkersResponseDto> responseDtoList = rentingMarkersService.findAll();
+
+        //then
+        assertThat(responseDtoList.size()).isEqualTo(2);
+        assertThat(responseDtoList.get(0).getMarkersId()).isEqualTo(1L);
+        assertThat(responseDtoList.get(1).getMarkersId()).isEqualTo(2L);
+    }
 }
