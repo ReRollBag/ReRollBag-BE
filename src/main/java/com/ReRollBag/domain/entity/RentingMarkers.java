732b2f0c3416f392b188f84b827d2845d778a8be
donghwan <wkazxf@gmail.com>
2023-03-19 14:06:00 +0900

    [FEAT] Renting Markers Entity, Repository Test Code (RRB-36)

---
+++ src/main/java/com/ReRollBag/domain/entity/RentingMarkers.java
@@ -0,0 +1,33 @@
+package com.ReRollBag.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class RentingMarkers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long markersId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "longitude")
+    private double longitude;
+
+    @Column(name = "maxBagsNum")
+    private int maxBagsNum;
+
+    @Column(name = "currentBagsNum")
+    private int currentBagsNum;
+}
