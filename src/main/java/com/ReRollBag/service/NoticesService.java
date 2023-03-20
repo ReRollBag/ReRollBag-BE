848b6bfe6ecf15688cde9c333007a068e72203d6
donghwan <wkazxf@gmail.com>
2023-03-20 16:49:31 +0900

    [FEAT] Notice Service : save (RRB-49)

---
+++ src/main/java/com/ReRollBag/service/NoticesService.java
@@ -0,0 +1,28 @@
+package com.ReRollBag.service;
+
+import com.ReRollBag.domain.dto.MockResponseDto;
+import com.ReRollBag.domain.dto.Notices.NoticesResponseDto;
+import com.ReRollBag.domain.dto.Notices.NoticesSaveRequestDto;
+import com.ReRollBag.domain.entity.Notices;
+import com.ReRollBag.repository.NoticesRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class NoticesService {
+    private static MockResponseDto responseDto = new MockResponseDto(true);
+    private final NoticesRepository noticesRepository;
+
+    @Transactional
+    public NoticesResponseDto save(NoticesSaveRequestDto requestDto) {
+        Notices notices = requestDto.toEntity();
+        noticesRepository.save(notices);
+        return new NoticesResponseDto(notices);
+    }
+
+}
