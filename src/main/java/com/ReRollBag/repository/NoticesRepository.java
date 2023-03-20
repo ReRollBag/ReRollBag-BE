e89f0b60ad1d35e154ab1b5e143a92a66ba26b8c
donghwan <wkazxf@gmail.com>
2023-03-20 16:49:13 +0900

    [FEAT] Notice Entity and Repository (RRB-49)

---
+++ src/main/java/com/ReRollBag/repository/NoticesRepository.java
@@ -0,0 +1,7 @@
+package com.ReRollBag.repository;
+
+import com.ReRollBag.domain.entity.Notices;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticesRepository extends JpaRepository<Notices, Long> {
+}
