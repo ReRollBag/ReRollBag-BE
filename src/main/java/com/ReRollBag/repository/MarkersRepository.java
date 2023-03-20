38466ceef9411e088004cc2658baef15c7871071
donghwan <wkazxf@gmail.com>
2023-03-20 15:27:18 +0900

    [FEAT] Delete Markers (RRB-36)

--- src/main/java/com/ReRollBag/repository/MarkersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ReRollBag.repository;
-
-import com.ReRollBag.domain.entity.Markers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarkersRepository extends JpaRepository<Markers, Long> {
-    public Markers findMarkersByName(String name);
-}
