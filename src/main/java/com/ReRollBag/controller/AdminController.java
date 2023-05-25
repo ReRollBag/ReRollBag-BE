ca80edfeb4a555db675f67146293c7f97646dfe3
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-05-25 13:56:55 +0900

    [FEAT] AdminController and AdminExceptionHandler : loginForAdmin, requestAdmin, verifyAdminRequestCertificationNumber

---
+++ src/main/java/com/ReRollBag/controller/AdminController.java
@@ -0,0 +1,34 @@
+package com.ReRollBag.controller;
+
+import com.ReRollBag.domain.dto.Admin.VerifyAdminRequestCertificationNumberDto;
+import com.ReRollBag.exceptions.adminExceptions.CertificationSignatureException;
+import com.ReRollBag.exceptions.adminExceptions.CertificationTimeExpireException;
+import com.ReRollBag.exceptions.adminExceptions.UserIsNotAdminException;
+import com.ReRollBag.exceptions.adminExceptions.UsersIsAlreadyAdminException;
+import com.ReRollBag.service.AdminService;
+import com.google.firebase.auth.FirebaseAuthException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class AdminController extends BaseController {
+    private final AdminService adminService;
+
+    @PostMapping("api/v2/users/loginForAdmin")
+    public ResponseEntity<?> loginForAdmin(@RequestHeader("token") String idToken) throws FirebaseAuthException, UserIsNotAdminException {
+        return sendResponseHttpByJson(adminService.loginForAdmin(idToken));
+    }
+
+    @PostMapping("api/v1/users/requestAdmin")
+    public ResponseEntity<?> requestAdmin(@RequestHeader("token") String jwtToken) throws UsersIsAlreadyAdminException {
+        adminService.requestAdmin(jwtToken);
+        return sendResponseHttpByJson("Successfully request Admin");
+    }
+
+    @PutMapping("api/v1/users/verifyAdminRequestCertificationNumber")
+    public ResponseEntity<?> verifyAdminRequestCertificationNumber(@RequestHeader("token") String jwtToken, @RequestBody VerifyAdminRequestCertificationNumberDto verifyAdminRequestCertificationNumberDto) throws CertificationTimeExpireException, CertificationSignatureException {
+        return sendResponseHttpByJson(adminService.verifyAdminRequestCertificationNumber(jwtToken, verifyAdminRequestCertificationNumberDto.getCertificationNumber(), verifyAdminRequestCertificationNumberDto.getRegion()));
+    }
+}
